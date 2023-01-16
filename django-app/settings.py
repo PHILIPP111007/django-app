9cd72df13912a313a46ef18ae6c5215792cfa152
PHILIPP111007 <r.phil@yandex.ru>
2023-01-17 01:54:08 +0300

    init

--- django-app/settings.py
+++ django-app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django-app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['app/app/templates'],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    'app/app/static'
+    'app/static'
 ]
 
 
